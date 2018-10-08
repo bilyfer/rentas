1091ee580d3afe98bdc23f4b33dd70ea45a3a5d6
bilyfer <bily.fernandez@unah.edu.hn>
2018-10-08 08:53:40 -0600

    Agregando boton para saludar

--- Rentas/Win.Rentas/Form1.cs
+++ Rentas/Win.Rentas/Form1.cs
@@ -16,5 +16,10 @@ namespace Win.Rentas
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola mundo");
+        }
     }
 }
