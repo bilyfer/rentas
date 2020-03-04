7b1fdfb927c895a8b1930b1e832b976e917ff725
bchavez <bchavez@aclaratech.com>
2020-03-03 18:06:29 -0600

    Ejercicio para practicar EF

---
+++ Rentas/Rentas.BL/Contexto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rentas.BL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto() : base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\RentaVideoJuegos.mdf")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}   
