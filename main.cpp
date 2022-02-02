5ef15e8d1fbd616389a355950998648d652f30d6
bilyfer <43960983+bilyfer@users.noreply.github.com>
2022-02-02 17:38:16 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  int a = 0;
+  int b = 0;
+  int c = 0;
+
+  cout << "Ingrese el valor de a: ";
+  cin >> a;
+  cout << "Ingrese el valor de b: ";
+  cin >> b;
+  // cout << "Ingrese el valor de c: ";
+  // cin >> c;
+
+  cout << endl;
+
+  // a = 10
+  // b = 70
+
+  if (a > b)
+  {
+    cout << "a es mayor"; // <- true
+  }
+  else
+  {
+    cout << "b es mayor"; // <- false
+  }
+
+
+
+
+
+
+
+
+
+
+
+  // // a = 5;
+  // // b = 7;
+  // // c = 30;
+
+  // if (a > b)
+  // {
+  //   cout << "a es el mayor";
+  // }
+  // else
+  // {
+  //   if (b > c)
+  //   {
+  //     cout << "b es el mayor";
+  //   }
+  //   else
+  //   {
+  //     cout << "c es el mayor";
+  //   }
+  // }
+
+
+  // //  true     false
+  // if (a > b && a > c)
+  // {
+  //   cout << "a es el mayor";
+  // }
+
+  // if (b > a && b > c)
+  // {
+  //   cout << "b es el mayor";
+  // }
+
+  // if (c > b && c > a)
+  // {
+  //   cout << "c es el mayor";
+  // }
+
+
+  // if (a > b)
+  // {
+  //   cout << "a es el mayor";
+  // }
+
+
+  // bool resultado = (a > b && a > c);
+
+  // if (a > b && a > c)
+  // {
+    
+  // }
+  
+
+  return 0;
+}
